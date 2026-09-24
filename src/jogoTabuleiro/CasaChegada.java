cda22afed3dc0bd9a50378cef9aca961861286a9
João Ítalo <italogadelha16@gmail.com>
2026-09-23 22:56:02 -0300

    Implementation of `montarCasas` + creation of `CasaChegada` to check for victory conditions.

---
+++ src/jogoTabuleiro/CasaChegada.java
@@ -0,0 +1,16 @@
+package jogoTabuleiro;
+
+/**
+ * Casa de chegada (40 / totalCasas): fim do jogo.
+ */
+public class CasaChegada extends Casa {
+
+    public CasaChegada(int numero) {
+        super(numero);
+    }
+
+    @Override
+    public void aplicarEfeito(Jogador jogador, Jogo jogo) {
+        // TODO: verificar condição de vitória
+    }
+}
